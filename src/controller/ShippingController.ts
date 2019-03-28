865d1033e88240daac7a5c31e47d63a4382256e5
Luan De Gregori <luan.gregori@compasso.com.br>
2019-03-28 15:49:56 -0300

    Pegando CPF, Data e valor Total da requisição

--- src/controller/ShippingController.ts
+++ src/controller/ShippingController.ts
@@ -5,23 +5,46 @@ const env = require('../config/env_vars')
 
 export async function Test(request: Request, response: Response) {
 
+    var items = request.body.request.order.items
+
+    var peso;
+    var m3; 
     
     
+    var data;
+    if(request.body.request.additionalProperties.dynamicProperties != 0){
+        data = request.body.request.additionalProperties.dynamicProperties[0].value
+    }else{
+        var date = new Date()
+        data = date.getDate() +"/"
+        if(date.getMonth() < 10){
+            data += "0" + (date.getMonth() + 1)  
+        }else{
+            data += (date.getMonth() + 1)
+        }
+        data += "/" + date.getFullYear()
+    }
+
+    console.log(data)
+
+
+    var value = request.body.request.order.orderTotal
+    var cep = request.body.request.address.postalCode
     var client = env.client;
 
     var url = env.url
     url += client.tip + client.cnpj + client.mun + client.est + client.seg
     url += "5/"                  //peso total
-    url += "100.00/"             //valor total
-    url += "0.025/"              //metors cubicos
-    url += "99010140/"           //cep destino
+    url += value + ".00/"            //valor total
+    url += "0.025/"              //metros cubicos
+    url += cep + "/"          //cep destino
     url += client.fil            //filial
-    url += "28/03/2019/"         //data dd/mm/aaaa
+    url += data + "/"         //data dd/mm/aaaa
     url += client.user           //usuario jameff
 
-    console.log(url)
+    //console.log(url)
 
-    var teste = await axios.get(url, {})
+    var res = await axios.get(url, {})
         .then(response => {
             return response.data
         })
@@ -29,5 +52,5 @@ export async function Test(request: Request, response: Response) {
             console.log(error)
         })
 
-    response.send(teste)
+    response.send(res)
 }
